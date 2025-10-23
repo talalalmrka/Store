6d215fa33d20f62ce798d41bc47f5f3ca4d5efbb
talalalmrka <talalminfo@gmail.com>
2025-10-23 19:13:04 +0300

    Update references in _App.cshtml to use built assets from the dist directory instead of Vite during development.

--- obj/Debug/net9.0/Store.AssemblyInfo.cs
+++ obj/Debug/net9.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e49e9fb5f1809d3fe67141baf5aff4ba7a8897")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
